fc1bf46b1ffa816025a6ef62905fdae69e0483ae
bzb <bzb@ppshorizon497.home>
2011-01-08 18:31:02 +0000

    distance

--- src/bzb/android/anywhere2/consumer/Consumer.java
+++ src/bzb/android/anywhere2/consumer/Consumer.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,11 +14,19 @@ import android.widget.TextView;
 
 public class Consumer extends Activity {
 	
+	private Handler mHandler = new Handler();
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
         final Button serviceButton = (Button) findViewById(R.id.serviceButton);
+        final Button hideButton = (Button) findViewById(R.id.hideButton);
+        hideButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				finish();
+			}
+		});
         
         if (LocationService.isRunning()) {
         	serviceButton.setText(R.string.serviceOff);
@@ -44,14 +53,51 @@ public class Consumer extends Activity {
 		            }
 				}
 			});
-        } else {
-        	Log.i(getClass().getName(),"GPS not enabled - enable it in the Settings menu");
         }
+        
+        new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					mHandler.post(new Runnable() {
+			            public void run() {
+							updateGPSStatusBlurb();
+						}
+			        });
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+        }).start();
+        
+        new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					mHandler.post(new Runnable() {
+			            public void run() {
+			            	updateContentStatusBlurb();
+			            }
+					});
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+        }).start();
     }
     
-    public void updateGPSStatusBlurb () {
+    private void updateGPSStatusBlurb () {
     	TextView gpsStatus = (TextView) findViewById(R.id.gpsStatus);
     	switch (LocationService.getGpsStatus()) {
+    	case OFF:
+    		gpsStatus.setText(R.string.gpsOffBlurb);
+    		break;
     	case STOPPED:
     		gpsStatus.setText(R.string.gpsStoppedBlurb);
     		break;
@@ -66,4 +112,24 @@ public class Consumer extends Activity {
     		break;
     	}
     }
+    
+   private void updateContentStatusBlurb () {
+    	TextView contentStatus = (TextView) findViewById(R.id.contentStatus);
+    	switch (LocationService.getContentStatus()) {
+    	case NO_LOCATION:
+    		contentStatus.setText(R.string.notSearchingForContentBlurb);
+    		break;
+    	case NEAR:
+    		String text = getString(R.string.nearContentBlurb) + " " + LocationService.getDistance() + " metres";
+    		contentStatus.setText(text);
+    		break;
+    	case DOWNLOADING:
+    		contentStatus.setText(R.string.downloadingContentBlurb);
+    		break;
+    	case STALE:
+    		contentStatus.setText(R.string.sameContentBlurb);
+    		break;
+    	}
+    }
+    
 }
--- src/bzb/android/anywhere2/consumer/LocationService.java
+++ src/bzb/android/anywhere2/consumer/LocationService.java
@@ -1,14 +1,30 @@
 package bzb.android.anywhere2.consumer;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.location.GpsStatus.Listener;
 import android.location.GpsSatellite;
 import android.location.GpsStatus;
+import android.location.GpsStatus.Listener;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -19,13 +35,28 @@ import android.util.Log;
 public class LocationService extends Service implements Listener, LocationListener {
 	
 	private static boolean running = false;
-	private static double lat = 0;
-	private static double lon = 0;
-	public static enum GPSSTATUS {STOPPED, STARTED_NOFIX, STARTED_FIX, STARTED_LOC};
+	public static enum GPSSTATUS {OFF, STOPPED, STARTED_NOFIX, STARTED_FIX, STARTED_LOC};
 	private static GPSSTATUS gpsStatus = GPSSTATUS.STOPPED;
+	public static enum CONTENTSTATUS {NO_LOCATION, NEAR, DOWNLOADING, STALE};
+	private static CONTENTSTATUS contentStatus = CONTENTSTATUS.NO_LOCATION;
+	private static int distance = -1;
 
+	private static final int NOTIFICATION_ID = 1;
+	
 	public void onCreate() {
 		super.onCreate();
+		
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		CharSequence tickerText = "Anywhere2 started";
+		long when = System.currentTimeMillis();
+		Notification notification = new Notification(R.drawable.ic_stat_notify, tickerText, when);
+		Context context = getApplicationContext();
+		CharSequence contentTitle = "Anywhere2 started";
+		CharSequence contentText = "Searching for nearby content";
+		Intent notificationIntent = new Intent(this, Consumer.class);
+		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+		notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+		mNotificationManager.notify(NOTIFICATION_ID, notification);
 		
 		setRunning(true);
 		
@@ -35,6 +66,7 @@ public class LocationService extends Service implements Listener, LocationListen
 			startTracking();
 		} catch (IOException e1) {
 			e1.printStackTrace();
+			stopSelf();
 		}
 	}
 	
@@ -52,9 +84,10 @@ public class LocationService extends Service implements Listener, LocationListen
 		if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
 			locationManager.addGpsStatusListener(this);
 			
-			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 30000, 0, this);
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 0, this);
 			Log.i(getClass().getName(),"Started GPS tracking");
 		} else {
+			setGpsStatus(GPSSTATUS.OFF);
 			Log.i(getClass().getName(),"GPS is not enabled; no GPS tracking");
 		}
 	}
@@ -89,12 +122,62 @@ public class LocationService extends Service implements Listener, LocationListen
 		}
 	}
 	
-	public void onLocationChanged(Location location) {
-		if (getGpsStatus() != GPSSTATUS.STARTED_LOC) {
-			setGpsStatus(GPSSTATUS.STARTED_LOC);
+	private boolean gettingURL = false;
+	
+	public void onLocationChanged(final Location location) {
+		setGpsStatus(GPSSTATUS.STARTED_LOC);
+		if (!gettingURL) {
+			gettingURL = true;
+			
+			Thread t = new Thread() {
+				public void run() {
+					HttpClient httpClient = new DefaultHttpClient();
+					HttpPost httpPost = new HttpPost("http://anywhere2gae.appspot.com/getcontent");
+					List <NameValuePair> parameters = new ArrayList <NameValuePair>();
+					parameters.add(new BasicNameValuePair("x",String.valueOf(location.getLatitude())));
+					parameters.add(new BasicNameValuePair("y",String.valueOf(location.getLongitude())));
+					UrlEncodedFormEntity sendentity;
+					try {
+						sendentity = new UrlEncodedFormEntity(parameters, HTTP.UTF_8);
+						httpPost.setEntity(sendentity);
+						HttpResponse response = httpClient.execute(httpPost);
+						ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+						response.getEntity().writeTo(ostream);
+
+						String resp = new String(ostream.toString());
+						try {
+							double distance = Double.parseDouble(resp);
+							setDistance(distance * 1000.0);
+							setContentStatus(CONTENTSTATUS.NEAR);
+						} catch (NumberFormatException e) {
+							String [] params = resp.split("@");
+							if(params.length > 1) {
+								setContentStatus(CONTENTSTATUS.DOWNLOADING);
+								//deal
+								//type = params[0];
+								//currentContentUrl = params[1];
+							} else {
+								System.out.println("Who knows");
+							}
+						}
+						
+					} catch (UnsupportedEncodingException e) {
+						e.printStackTrace();
+						Log.d("catch1",e.getMessage());
+					} catch (ClientProtocolException e) {
+						e.printStackTrace();
+						Log.d("catch2",e.getMessage());
+					} catch (IOException e) {
+						e.printStackTrace();
+						Log.d("catch3",e.getMessage());
+					} finally {
+						gettingURL = false;
+					}
+				}
+			};
+			t.start();
 		}
-		setLat(location.getLatitude());
-		setLon(location.getLongitude());
+		
 		Log.i(getClass().getName(), "GPS location changed: lat="+location.getLatitude()+", lon="+location.getLongitude());
 	}
 
@@ -108,11 +191,16 @@ public class LocationService extends Service implements Listener, LocationListen
 	}
 	
 	public void onDestroy() {
+		NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		mNotificationManager.cancel(NOTIFICATION_ID);
+		
 		if (locationManager != null) {
 			locationManager.removeGpsStatusListener(this);
 			locationManager.removeUpdates(this);
 			Log.i(getClass().getName(), "Unregistered GPS listeners");
 		}
+		
+		setGpsStatus(GPSSTATUS.STOPPED);
 	
 		super.onDestroy();
 
@@ -123,23 +211,7 @@ public class LocationService extends Service implements Listener, LocationListen
 	
 	public IBinder onBind(Intent i) {return null;}
 
-	public static void setLat(double lat) {
-		LocationService.lat = lat;
-	}
-
-	public static double getLat() {
-		return lat;
-	}
-
-	public static void setLon(double lon) {
-		LocationService.lon = lon;
-	}
-
-	public static double getLon() {
-		return lon;
-	}
-
-	public static void setGpsStatus(GPSSTATUS gpsStatus) {
+	private void setGpsStatus(GPSSTATUS gpsStatus) {
 		LocationService.gpsStatus = gpsStatus;
 	}
 
@@ -153,6 +225,22 @@ public class LocationService extends Service implements Listener, LocationListen
 
 	public static boolean isRunning() {
 		return running;
+	}
+
+	public static void setDistance(double distance) {
+		LocationService.distance = (int) distance;
+	}
+
+	public static int getDistance() {
+		return distance;
+	}
+
+	public static void setContentStatus(CONTENTSTATUS contentStatus) {
+		LocationService.contentStatus = contentStatus;
+	}
+
+	public static CONTENTSTATUS getContentStatus() {
+		return contentStatus;
 	}
 
 	
